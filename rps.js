93eee36407f9a0c579b4f64fe9f2645b44b5cb61
johnedwardlee <50990111+johnedwardlee@users.noreply.github.com>
2023-04-20 00:03:44 -0400

    Update with basic UI layout: Buttons trigger event funcitons Beginnings of a scoreboard in html list format

--- rps.js
+++ rps.js
@@ -1,68 +1,92 @@
+// GLOBALS
 CHOICES = ['rock', 'paper', 'scissors'];
 const NUMBER_OF_GAMES = 5;
+let scorePlayer = 0;
+let scoreComp = 0;
+let gameCounter = 0;
 
-let getComputerChoice = function () {
+// FUNCTIONS
+function getComputerChoice() {
   return CHOICES[Math.floor(Math.random() * CHOICES.length)];
-};
+}
 
-let playRound = function (compChoice, playerChoice) {
+function roundResult(result, msg) {
+  if (result == 'player') {
+    scorePlayer++;
+    playerScoreDisp.textContent = scorePlayer;
+  } else if (result == 'computer') {
+    scoreComp++;
+    compScoreDisp.textContent = scoreComp;
+  }
+  gameCount.textContent = gameCounter;
+}
+
+function playRound(e) {
+  let playerChoice = this.textContent;
+  const compChoice = getComputerChoice();
+
   if (typeof playerChoice == 'string')
     playerChoice = playerChoice.toLowerCase();
   else {
     alert(
       'You have to input something to play the game.\nTry to be competent next time...'
     );
-    return ['bad', 'Try Again'];
+    roundResult('bad', 'Try Again');
   }
 
-  if (!CHOICES.includes(playerChoice)) {
-    alert(
-      'You had only three options and you typed none of them.\nRefresh to try agian at being competent... '
-    );
-    return ['bad', 'Try Again'];
-  }
-
+  gameCounter++;
   if (playerChoice == compChoice) {
-    return ['draw', "It's a draw, you both chose " + playerChoice];
+    roundResult('draw', "It's a draw, you both chose " + playerChoice);
   } else if (playerChoice == 'rock') {
     if (compChoice == 'scissors')
-      return ['player', 'You win, ' + playerChoice + ' beats ' + compChoice];
+      roundResult(
+        'player',
+        'You win, ' + playerChoice + ' beats ' + compChoice
+      );
     else if (compChoice == 'paper')
-      return ['computer', 'You lose ' + compChoice + ' beats ' + playerChoice];
+      roundResult(
+        'computer',
+        'You lose ' + compChoice + ' beats ' + playerChoice
+      );
   } else if (playerChoice == 'paper') {
     if (compChoice == 'rock')
-      return ['player', 'You win, ' + playerChoice + ' beats ' + compChoice];
+      roundResult(
+        'player',
+        'You win, ' + playerChoice + ' beats ' + compChoice
+      );
     else if (compChoice == 'scissors')
-      return ['computer', 'You lose ' + compChoice + ' beats ' + playerChoice];
+      roundResult(
+        'computer',
+        'You lose ' + compChoice + ' beats ' + playerChoice
+      );
   } else if (playerChoice == 'scissors') {
     if (compChoice == 'paper')
-      return ['player', 'You win, ' + playerChoice + ' beats ' + compChoice];
+      roundResult(
+        'player',
+        'You win, ' + playerChoice + ' beats ' + compChoice
+      );
     else if (compChoice == 'rock')
-      return ['computer', 'You lose ' + compChoice + ' beats ' + playerChoice];
-  } else return ['bad', 'You really goofed something up...'];
-};
-
-let scorePlayer = 0;
-let scoreComputer = 0;
-for (let i = 0; i < NUMBER_OF_GAMES; i++) {
-  let playerChoiceInput = prompt(
-    'Choose your weapon:\nRock, Paper, or Scissors'
-  );
-  let result = playRound(getComputerChoice(), playerChoiceInput);
-
-  if (result[0] == 'player') scorePlayer++;
-  else if (result[0] == 'computer') scoreComputer++;
-  else if (result[0] == 'bad') i--;
-
-  console.log(
-    `${result[1]}\nThe Score is now:\nYou: ${scorePlayer}\nComputer: ${scoreComputer}`
-  );
+      roundResult(
+        'computer',
+        'You lose ' + compChoice + ' beats ' + playerChoice
+      );
+  } else roundResult('bad', 'You really goofed something up...');
 }
 
-if (scoreComputer > scorePlayer) {
-  console.log('You lost, as expected...');
-} else if (scorePlayer > scoreComputer) {
-  console.log('You won somehow!');
-} else {
-  console.log('No winner, which means you still lost...');
-}
+// MAIN
+const choiceButtons = document.querySelectorAll('.choice');
+choiceButtons.forEach((choice) => choice.addEventListener('click', playRound));
+const gameCount = document.querySelector('.game-count');
+const playerScoreDisp = document.querySelector('.player-score');
+const compScoreDisp = document.querySelector('.comp-score');
+playerScoreDisp.textContent = scorePlayer;
+compScoreDisp.textContent = scoreComp;
+gameCount.textContent = gameCounter;
+
+// if (scoreComp > scorePlayer) {
+//   console.log('You lost, as expected...');
+// } else if (scorePlayer > scoreComp) {
+//   console.log('You won somehow!');
+// } else {
+//   console.log('No winner, which means you still lost...');
+// }
