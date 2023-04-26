b9392178c5fa2c1eb2d86329b85c2327d1419600
johnedwardlee <50990111+johnedwardlee@users.noreply.github.com>
2023-04-26 00:10:02 -0400

    Big Update: Added images Added some button animations Created a css file to customize the elements Added button to reset game(not working yet) Added conditions for a 5 round game to end Working on text scroll messages as a way to deliver the player messages

--- rps.js
+++ rps.js
@@ -6,6 +6,11 @@ let scoreComp = 0;
 let gameCounter = 0;
 
 // FUNCTIONS
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('clicked');
+}
+
 function getComputerChoice() {
   return CHOICES[Math.floor(Math.random() * CHOICES.length)];
 }
@@ -13,26 +18,19 @@ function getComputerChoice() {
 function roundResult(result, msg) {
   if (result == 'player') {
     scorePlayer++;
-    playerScoreDisp.textContent = scorePlayer;
+    playerScoreDisp.textContent = `Player: ${scorePlayer}`;
   } else if (result == 'computer') {
     scoreComp++;
-    compScoreDisp.textContent = scoreComp;
+    compScoreDisp.textContent = `Computer: ${scoreComp}`;
   }
-  gameCount.textContent = gameCounter;
+  gameCount.textContent = `Game Count: ${gameCounter}`;
 }
 
 function playRound(e) {
-  let playerChoice = this.textContent;
+  let playerChoice = this.classList[0];
+  const choice = document.querySelector(`.${playerChoice}`);
+  choice.classList.add('clicked');
   const compChoice = getComputerChoice();
-
-  if (typeof playerChoice == 'string')
-    playerChoice = playerChoice.toLowerCase();
-  else {
-    alert(
-      'You have to input something to play the game.\nTry to be competent next time...'
-    );
-    roundResult('bad', 'Try Again');
-  }
 
   gameCounter++;
   if (playerChoice == compChoice) {
@@ -71,22 +69,32 @@ function playRound(e) {
         'You lose ' + compChoice + ' beats ' + playerChoice
       );
   } else roundResult('bad', 'You really goofed something up...');
+
+  if (gameCounter == NUMBER_OF_GAMES) {
+    const gameOver = document.createElement('div');
+    gameOver.classList.add('game-over');
+    let gameOverMsg;
+    if (scorePlayer > scoreComp)
+      gameOverMsg = "You won, maybe you aren't as incompetent as I thought...";
+    else
+      gameOverMsg =
+        'I figured you would lose. Did you actually think you had a chance?';
+    const gameOverTextNode = document.createTextNode(gameOverMsg);
+    gameOver.appendChild(gameOverTextNode);
+    const topOfPage = document.getElementById('btn-container');
+    document.body.insertBefore(gameOver, topOfPage);
+  }
 }
 
 // MAIN
 const choiceButtons = document.querySelectorAll('.choice');
 choiceButtons.forEach((choice) => choice.addEventListener('click', playRound));
+choiceButtons.forEach((choice) =>
+  choice.addEventListener('transitionend', removeTransition)
+);
 const gameCount = document.querySelector('.game-count');
 const playerScoreDisp = document.querySelector('.player-score');
 const compScoreDisp = document.querySelector('.comp-score');
-playerScoreDisp.textContent = scorePlayer;
-compScoreDisp.textContent = scoreComp;
-gameCount.textContent = gameCounter;
-
-// if (scoreComp > scorePlayer) {
-//   console.log('You lost, as expected...');
-// } else if (scorePlayer > scoreComp) {
-//   console.log('You won somehow!');
-// } else {
-//   console.log('No winner, which means you still lost...');
-// }
+playerScoreDisp.textContent = `Player: ${scorePlayer}`;
+compScoreDisp.textContent = `Computer: ${scoreComp}`;
+gameCount.textContent = `Game Count: ${gameCounter}`;
